e64a58598997d8a6657dfb28e2afebb04c43b363
Timo Bingmann <tbgit@panthema.net>
2018-10-14 12:14:55 +0000

    OctoWS2811 pinout list

--- lib/NeoAnimation/Strip/OctoSK6812Adapter.hpp
+++ lib/NeoAnimation/Strip/OctoSK6812Adapter.hpp
@@ -11,6 +11,24 @@
 
 #include <NeoAnimation/Strip/LEDStripBase.hpp>
 
+/******************************************************************************/
+
+/*
+  Required Connections
+  --------------------
+    pin 2:  LED Strip #1    OctoWS2811 drives 8 LED Strips.
+    pin 14: LED strip #2    All 8 are the same length.
+    pin 7:  LED strip #3
+    pin 8:  LED strip #4    A 100 ohm resistor should used
+    pin 6:  LED strip #5    between each Teensy pin and the
+    pin 20: LED strip #6    wire to the LED strip, to minimize
+    pin 21: LED strip #7    high frequency ringining & noise.
+    pin 5:  LED strip #8
+    pin 15 & 16 - Connect together, but do not use
+    pin 4 - Do not use
+    pin 3 - Do not use as PWM.  Normal use is ok.
+*/
+
 #include <OctoSK6812.h>
 
 namespace NeoAnimation {
