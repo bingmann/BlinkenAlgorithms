ab7e9fe533b20d3c7d39d4a100e961b72929b3e4
Timo Bingmann <tbgit@panthema.net>
2019-07-20 19:33:14 +0200

    blinken-sort-teensy version

---
+++ blinken-sort-teensy/src/blinken-sort.cpp
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * blinken-sort-teensy/src/blinken-sort.cpp
+ *
+ * Copyright (C) 2019 Timo Bingmann <tb@panthema.net>
+ *
+ * All rights reserved. Published under the GNU General Public License v3.0
+ ******************************************************************************/
+
+#include <Arduino.h>
+
+#include <BlinkenAlgorithms/Porting/Teensy.hpp>
+
+#include <BlinkenAlgorithms/Animation/RandomAlgorithm.hpp>
+#include <BlinkenAlgorithms/Strip/OctoSK6812Adapter.hpp>
+
+using namespace BlinkenAlgorithms;
+
+/******************************************************************************/
+
+static const size_t strip_size = 300;
+
+DMAMEM int displayMemory[strip_size * 8];
+int drawMemory[strip_size * 8];
+
+OctoSK6812 base_strip(strip_size, displayMemory, drawMemory, SK6812_GRBW);
+
+using Strip = OctoSK6812Adapter<OctoSK6812>;
+Strip strip(base_strip, /* active_parts */ 1);
+
+/******************************************************************************/
+
+bool g_terminate = false;
+size_t g_delay_factor = 1000;
+
+void delay_poll() { }
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println("Starting...");
+
+    unsigned long seed = 0;
+    for (int i = 0; i < 32; i++) {
+        seed ^= ((analogRead(A0) & 0x01) << i);
+    }
+    randomSeed(seed);
+    srandom(seed);
+
+    base_strip.begin();
+}
+
+void loop() {
+    RunRandomAlgorithmAnimation(strip);
+}
+
+/******************************************************************************/
