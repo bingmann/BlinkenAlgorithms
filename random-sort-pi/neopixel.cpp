71df8c733194bf2cf0c7c713a8bcb8ceb0869225
Timo Bingmann <tbgit@panthema.net>
2019-07-20 15:41:15 +0200

    add library.json and fix include in platformio.ini

--- random-sort-pi/neopixel.cpp
+++ random-sort-pi/neopixel.cpp
@@ -14,6 +14,7 @@ using namespace NeoAnimation;
 PiSPI_APA102 my_strip("/dev/spidev0.0", /* strip_size */ 5 * 96);
 
 bool g_terminate = false;
+size_t g_delay_factor = 1000;
 
 int main() {
     srandom(time(nullptr));
--- random-sort-sound-teensy/src/neopixel.cpp
+++ random-sort-sound-teensy/src/neopixel.cpp
@@ -62,6 +62,7 @@ AudioControlSGTL5000 audioShield;
 /******************************************************************************/
 
 bool g_terminate = false;
+size_t g_delay_factor = 1000;
 
 void setup() {
     Serial.begin(115200);
